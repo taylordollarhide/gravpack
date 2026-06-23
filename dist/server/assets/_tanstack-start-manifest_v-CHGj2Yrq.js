b321a6a7a67b6f27a68a6799883d41017b9523e7
Taylor Dollarhide <t.p.dollarhide@gmail.com>
2026-06-23 13:56:06 -0600

    Prevent badge text from wrapping

---
+++ dist/server/assets/_tanstack-start-manifest_v-CHGj2Yrq.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/Users/taylordollarhide/Library/Mobile Documents/com~apple~CloudDocs/DollarhideDesign/gravpack-app/src/routes/__root.tsx", children: ["/"], assets: [{ tag: "link", attrs: { rel: "stylesheet", href: "/assets/index-fCWQh6xk.css", type: "text/css" } }], preloads: ["/assets/index-DeetpDta.js"] }, "/": { filePath: "/Users/taylordollarhide/Library/Mobile Documents/com~apple~CloudDocs/DollarhideDesign/gravpack-app/src/routes/index.tsx", children: void 0, assets: [], preloads: ["/assets/index-DAUMotvx.js"] } }, clientEntry: "/assets/index-DeetpDta.js" });
+export {
+  tsrStartManifest
+};
--- dist/server/server.js
+++ dist/server/server.js
@@ -73,7 +73,7 @@ function getResponse() {
 }
 var HEADERS = { TSS_SHELL: "X-TSS_SHELL" };
 async function getStartManifest(matchedRoutes) {
-  const { tsrStartManifest } = await import("./assets/_tanstack-start-manifest_v-EXJTj2Eq.js");
+  const { tsrStartManifest } = await import("./assets/_tanstack-start-manifest_v-CHGj2Yrq.js");
   const startManifest = tsrStartManifest();
   const rootRoute = startManifest.routes[rootRouteId] = startManifest.routes[rootRouteId] || {};
   rootRoute.assets = rootRoute.assets || [];
@@ -675,7 +675,7 @@ var baseManifestPromise;
 var cachedFinalManifestPromise;
 async function loadEntries() {
   const [routerEntry, startEntry, pluginAdapters] = await Promise.all([
-    import("./assets/router-bbFBM6E-.js"),
+    import("./assets/router-CIynDqvg.js"),
     import("./assets/start-HYkvq4Ni.js"),
     import("./assets/__23tanstack-start-plugin-adapters-Cwee5PKy.js")
   ]);
