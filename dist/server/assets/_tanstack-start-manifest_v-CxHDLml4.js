06a8411b1c732ae2ce260e69e99da27e43ed2a10
Taylor Dollarhide <t.p.dollarhide@gmail.com>
2026-06-23 10:32:09 -0600

    Center stat cards on shelf screen

---
+++ dist/server/assets/_tanstack-start-manifest_v-CxHDLml4.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/Users/taylordollarhide/Library/Mobile Documents/com~apple~CloudDocs/DollarhideDesign/gravpack-app/src/routes/__root.tsx", children: ["/"], assets: [{ tag: "link", attrs: { rel: "stylesheet", href: "/assets/index-Bk3QPZGY.css", type: "text/css" } }], preloads: ["/assets/index-CbS__Km3.js"] }, "/": { filePath: "/Users/taylordollarhide/Library/Mobile Documents/com~apple~CloudDocs/DollarhideDesign/gravpack-app/src/routes/index.tsx", children: void 0, assets: [], preloads: ["/assets/index-Cd1x2ggK.js"] } }, clientEntry: "/assets/index-CbS__Km3.js" });
+export {
+  tsrStartManifest
+};
